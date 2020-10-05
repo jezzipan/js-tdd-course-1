f2af28cd0648772233330aa47f27ab2d19d88e8e
jezzipan <jessica.leute@gmail.com>
2020-10-05 12:56:12 -0300

    iniciando teste

---
+++ src/main.js
@@ -0,0 +1,5 @@
+export const search = () => {}
+export const searchAlbums = () => {}
+export const searchArtist = () => {}
+export const searchTracks = () => {}
+export const searchPlaylist = () => {}
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,24 @@
 import { expect } from 'chai';
+import { search, searchAlbums, searchArtist, searchTracks, searchPlaylist} from '../src/main';
+describe('SpotIfood - Test', () => {
+    
+    describe('smoke tests', () => {
 
-describe('Main', () => {
+        it('should exist the search method', () =>{
+            expect(search).to.exist;
+        });
+        it('should exist the searchAlbums method', () =>{
+            expect(searchAlbums).to.exist;
+        });
+        it('should exist the searchArtist method', () =>{
+            expect(searchArtist).to.exist;
+        });
+        it('should exist the searchTracks method', () =>{
+            expect(searchTracks).to.exist;
+        });
+        it('should exist the searchPlaylist method', () =>{
+            expect(searchPlaylist).to.exist;
+        });
+    });
 
 });
